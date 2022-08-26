80fd513fccafe8824443eef12051b6faee806c24
Matthew-Graves-Git <57644194+Matthew-Graves-Git@users.noreply.github.com>
2022-08-26 19:50:32 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import logo from './logo.svg';
+import './App.css';
+import Square from './components/BingoSquare';
+
+function App() {
+  return (
+    <h1>
+    <div className="grid-container">
+    <div><Square  className="grid-item"/></div>
+    <div><Square  className="grid-item"/></div>
+    <div><Square  className="grid-item"/></div>
+    <div><Square  className="grid-item"/></div>
+    <div><Square  className="grid-item"/></div>
+    <div><Square  className="grid-item"/></div>
+    <div><Square  className="grid-item"/></div>
+    <div><Square  className="grid-item"/></div>
+    <div><Square  className="grid-item"/></div>
+    <div><Square  className="grid-item"/></div>
+    <div><Square  className="grid-item"/></div>
+    <div><Square  className="grid-item"/></div>
+    <div><Square  className="grid-item"/></div>
+    <div><Square  className="grid-item"/></div>
+    <div><Square  className="grid-item"/></div>
+    <div><Square  className="grid-item"/></div>
+    <div><Square  className="grid-item"/></div>
+    <div><Square  className="grid-item"/></div>
+    <div><Square  className="grid-item"/></div>
+    <div><Square  className="grid-item"/></div>
+    <div><Square  className="grid-item"/></div>
+    <div><Square  className="grid-item"/></div>
+    <div><Square  className="grid-item"/></div>
+    <div><Square  className="grid-item"/></div>
+    <div><Square  className="grid-item"/></div>
+
+    
+    
+
+    </div>
+    </h1>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,25 @@
+
+
+// const root = ReactDOM.createRoot(
+//     document.getElementById('root')
+//   );
+//   root.render(<Square/>);
+//   root.render(<Square/>);
+
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import Board from './components/BingoBoard';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <Board/>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
